703e2cd770846cd04cbc592459d2d9d59cb138bd
Pot2709 <jordan.potenciano@ismem.edu.pe>
2024-10-31 14:09:27 -0500

    aseeee

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import 'flowbite';
 
 // Control de visibilidad del menú en pantallas pequeñas
+// Control de visibilidad del menú en pantallas pequeñas
 document.getElementById('menu-toggle').addEventListener('click', function() {
-    const menu = document.getElementById('hs-navbar-header-floating');
+    const menu = document.getElementById('menu-items'); // Asegúrate de usar el ID correcto
     menu.classList.toggle('hidden');
 });
+
+
